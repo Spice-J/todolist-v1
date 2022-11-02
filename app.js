f439fad796625422c6b22717a0107bc6522529b9
Jared <spicejared@gmail.com>
2022-11-02 17:42:14 -0400

    set up todolist-v1 project files and write app.js template code

---
+++ app.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("hello");
+});
+
+
+
+
+
+
+app.listen(3000, function(req, res) {
+    console.log("Server started on port 3000.");
+});
