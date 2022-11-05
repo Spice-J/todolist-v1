41aed99268335b03ffdd29a112ef55eb0bbd33bb
Jared <spicejared@gmail.com>
2022-11-05 16:36:32 -0400

    Add public directory with css directory containing styles.css

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 
 let items = ["buy food", "cook food", "eat food"];
 
