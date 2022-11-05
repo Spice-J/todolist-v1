0a986cdeddab3b78387879b711a7ce67d116fd7e
Jared <spicejared@gmail.com>
2022-11-05 18:42:27 -0400

    Apply classes to list.ejs elements to incorporate css styles.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 let items = ["buy food", "cook food", "eat food"];
 
