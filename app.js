fb209763de83effcfd7c7113090c3b4cfcf09978
Jared <spicejared@gmail.com>
2022-11-05 11:04:48 -0400

    Shorten syntax in app.js line 10 and 11 (var currentDay)

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
+  var currentDay = new Date().getDay();
   var day = "";
 
   switch (currentDay) {
