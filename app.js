433673ce59472cc26b17a423f5b58a4166a47970
Jared <spicejared@gmail.com>
2022-11-05 10:59:31 -0400

    Add views/list.ejs file. Create switch statement in app.js to use day of week as ejs variable. Use app.render in place of app.write.

--- app.js
+++ app.js
@@ -4,24 +4,43 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(req, res) {
+app.set("view engine", "ejs");
 
-    var today = new Date();
-    var currentDay = today.getDay();
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-    if (currentDay === 6 || currentDay === 0) {
-        res.write("<h1>Yay it's the weekend!</h1>");
-    } else {
-        res.sendFile(__dirname + "/index.html");
-    }
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
 
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
+  }
+
+  res.render("list", { kindOfDay: day });
 });
 
-
-
-
-
-
-app.listen(3000, function(req, res) {
-    console.log("Server started on port 3000.");
+app.listen(3000, function (req, res) {
+  console.log("Server started on port 3000.");
 });
